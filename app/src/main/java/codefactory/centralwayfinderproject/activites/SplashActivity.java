9033eb693c239e1adf88f4bd50881f23885d4367
ChuckUFarley <040107741@my.central.wa.edu.au>
2015-09-17 16:01:52 +0800

    added web service check (WIP)

--- app/src/main/java/codefactory/centralwayfinderproject/activites/SplashActivity.java
+++ app/src/main/java/codefactory/centralwayfinderproject/activites/SplashActivity.java
@@ -8,18 +8,40 @@ import android.content.Intent;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
 
 import codefactory.centralwayfinderproject.R;
 
 public class SplashActivity extends Activity implements OnClickListener {
 
     private Button btn_startApp;
+
+    //SOAP vars
+    private final String NAMESPACE = "http://tempuri.org/"; //namespace is derived from the ?wsdl
+    private final String URL = "http://student.mydesign.central.wa.edu.au/cf_Wayfinding_WebService/WF_Service.svc";
+    //check service conn
+    private final String SOAP_ACTION_CHECK_SERVICE_CONN = "http://tempuri.org/WF_Service_Interface/checkServiceConn";
+    private final String METHOD_CHECK_SERVICE_CONN = "checkServiceConn";
+    //also check db conn
+    private final String SOAP_ACTION_CHECK_DB_CONN = "http://tempuri.org/WF_Service_Interface/checkDBConn";
+    private final String METHOD_CHECK_DB_CONN = "checkDBConn";
+
+    boolean checkServiceResult;
+    boolean checkServiceExceptionCaught;
+    String exceptionString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +69,10 @@ public class SplashActivity extends Activity implements OnClickListener {
             noGpsConnectionDialog();
             Log.d("Not GPS", "GPS IS OFF");
         }
+
+        AsyncCallWSCheckServiceConn checkServiceConnAST = new AsyncCallWSCheckServiceConn();
+        checkServiceConnAST.execute();
+
     }
 
 
@@ -159,6 +185,7 @@ public class SplashActivity extends Activity implements OnClickListener {
 
 
 
+
     /**
      * Checks for an GPS Connection
      * @return Boolean
@@ -170,5 +197,78 @@ public class SplashActivity extends Activity implements OnClickListener {
         return lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
     }
+
+
+    //check web service connection
+    private class AsyncCallWSCheckServiceConn extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            checkServiceConn();
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            if (!checkServiceResult){
+                Toast.makeText(SplashActivity.this, "Error Connecting to web service", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+
+        }
+
+        public void checkServiceConn() {
+
+            //theres probably a better way to pass around results but these global bools work ftm
+            //i think rather than using public global vars i should create an interface
+            checkServiceResult = false;
+            checkServiceExceptionCaught = false;
+
+            //Create request
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_CHECK_SERVICE_CONN);
+
+            //Create envelope
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+                    SoapEnvelope.VER11);
+            envelope.dotNet = true;
+            //Set output SOAP object
+            envelope.setOutputSoapObject(request);
+            //Create HTTP call object
+            HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+
+            try {
+                //Invoke web service
+                androidHttpTransport.call(SOAP_ACTION_CHECK_SERVICE_CONN, envelope);
+                //Get the response
+                //using soapprimitive for a simple primitive (in this case boolean) response,
+                //if it was an object or anything with multiple elements we'd use soapobject
+                SoapPrimitive response = (SoapPrimitive) envelope.getResponse();
+                //Assign response
+                checkServiceResult = Boolean.valueOf(response.toString());
+                Log.d("Soap action getDBconn:", "SUCCESS");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                checkServiceResult = false;
+                checkServiceExceptionCaught = true;
+                Log.d("Soap action getDBconn:", "FAIL");
+                exceptionString = e.toString();
+            }
+
+        }
+
+    }
+
+
 
 }
